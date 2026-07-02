c3cdba855ccbeb6c987d3205bd2337f6559a6163
shubhup2211 <9867539049sp@gmail.com>
2026-07-02 11:45:25 +0530

    todays code

--- JQTest.js
+++ JQTest.js
@@ -74,4 +74,78 @@ $(document).ready(function(){
          }
     });
 
+
+
+    $("#dept").on("change", function(){
+
+        var deptVal = $("#dept").val();
+        var empdrop = $("#emp");
+
+        var empList = '<option value="">-- Select emp list--</option>';
+
+        if(deptVal=="IT"){
+            empList += '<option value="1">Amit</option>';
+            empList += '<option value="2">Sahil</option>';
+        } else if(deptVal=="HR"){
+            empList += '<option value="3">Shubham</option>';
+            empList += '<option value="4">Aditya</option>';
+        }
+
+
+        if(deptVal != "" ){
+            empdrop.html(empList);
+        } else{
+            empdrop.html('<option value="">-- Select emp list--</option>');
+        }
+    });
+
+    $("#txtip").on("input change", function(){
+
+        var ui = $("#txtip").val();
+        var para = $("#para");
+        var color = "sty"
+        
+        para.text(ui);
+
+        $("#head").css("color",ui);
+
+
+    });
+
+
+    var arrEmp = [
+        {name:'Shubham', id:101},
+        {name:'John', id:102}];
+
+
+    $("#tbtn").click(function(){
+
+        var obj='';
+
+        //for loop
+
+        // for(var i=0; i<arrEmp.length; i++){
+
+        //     var name = (arrEmp[i].name);
+        //     var id = (arrEmp[i].id);
+        //     obj += "<tr>";
+        //     obj += `<td>${name}</td>`;
+        //     obj += `<td>${id}</td>`;
+        //     obj += "</tr>";
+        // }
+
+
+        //for each loop
+
+        $.each(arrEmp,function(index,item){
+
+            obj += "<tr>";
+            obj += `<td>${item.name}</td>`;
+            obj += `<td>${item.id}</td>`;
+            obj += "<tr>";
+        });
+
+          $("#bootTable").html(obj);          
+
+    });   
 });
